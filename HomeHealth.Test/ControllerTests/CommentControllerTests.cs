53c9607b60ded8e58d99fef01cf9950bbe051433
Troy Anderson <TroyAnderson.d@gmail.com>
2020-11-11 03:53:07 -0500

    removed api tests

--- HomeHealth.Test/ControllerTests/CommentControllerTests.cs
+++ HomeHealth.Test/ControllerTests/CommentControllerTests.cs
@@ -1,55 +1,55 @@
-using System.Threading.Tasks;
-using System.Net;
-using System.Net.Http;
-using System.Collections.Generic;
-using HomeHealth.Web.Entities;
-using Xunit;
-using FluentAssertions;
-using HomeHealth;
-using Microsoft.AspNetCore.Mvc.Testing;
-//
-using HomeHealth.Test;
+// using System.Threading.Tasks;
+// using System.Net;
+// using System.Net.Http;
+// using System.Collections.Generic;
+// using HomeHealth.Web.Entities;
+// using Xunit;
+// using FluentAssertions;
+// using HomeHealth;
+// using Microsoft.AspNetCore.Mvc.Testing;
+// //
+// using HomeHealth.Test;
 
-namespace HomeHealth.Test.Controllers
-{
-    public class ApiTests : IClassFixture<CustomWebApplicationFactory<Startup>>
-    {
-        private readonly HttpClient Testclient;
+// namespace HomeHealth.Test.Controllers
+// {
+//     public class ApiTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+//     {
+//         private readonly HttpClient Testclient;
 
-        // private readonly WebApplicationFactory<Startup> _factory;
+//         // private readonly WebApplicationFactory<Startup> _factory;
         
-        public ApiTests(CustomWebApplicationFactory<Startup> factory) {
-            Testclient = factory.CreateClient();
+//         public ApiTests(CustomWebApplicationFactory<Startup> factory) {
+//             Testclient = factory.CreateClient();
 
-        }
-        [Theory]
-        [InlineData("/api/comments")]
-        [InlineData("/api/Charges")]
-        [InlineData("/api/Messages")]
-        [InlineData("/api/Professional_Service")]
-        [InlineData("/api/Appointments")]
-        [InlineData("/api/Professionals")]
-        [InlineData("/api/Service")]
-        public async Task GetAll_Test(string url){
-            //Arrange
+//         }
+//         [Theory]
+//         [InlineData("/api/comments")]
+//         [InlineData("/api/Charges")]
+//         [InlineData("/api/Messages")]
+//         [InlineData("/api/Professional_Service")]
+//         [InlineData("/api/Appointments")]
+//         [InlineData("/api/Professionals")]
+//         [InlineData("/api/Service")]
+//         public async Task GetAll_Test(string url){
+//             //Arrange
         
-            //Act
+//             //Act
 
-            var response = await Testclient.GetAsync(url);
+//             var response = await Testclient.GetAsync(url);
 
-            //Assert
+//             //Assert
 
-            // Console.WriteLine(response.StatusCode);
-            // Assert.Equal(HttpStatusCode.OK,response.StatusCode);
+//             // Console.WriteLine(response.StatusCode);
+//             // Assert.Equal(HttpStatusCode.OK,response.StatusCode);
 
             
-            // response.Content.ReadAsASync().Be(HttpStatusCode.OK);
+//             // response.Content.ReadAsASync().Be(HttpStatusCode.OK);
 
-            response.StatusCode.Should().Be(HttpStatusCode.OK);
-            // response.Content.ReadAsASync<List<Comme>>
+//             response.StatusCode.Should().Be(HttpStatusCode.OK);
+//             // response.Content.ReadAsASync<List<Comme>>
             
-        }
+//         }
         
-    }
+//     }
     
-}
+// }
